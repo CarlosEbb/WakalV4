2cdd1b9aacfb02e41cdf06aba79b5e6c31e1498b
CarlosEbb <32774791+CarlosEbb@users.noreply.github.com>
2024-04-10 13:35:44 -0400

    dashboard adelanto

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,17 @@ export default {
 				'custom-primary': '#7574b5',
 				'custom-secundary': '#464473',
 				'custom-tertiary': '#14275b',
-				'custom-quaternary': '#eae6f4',
+				'custom-quaternary': '#eff0f4',
+				'custom-quinary': '#b7bfe3',
+				'custom-senary': '#8e65a9',
+				'custom-septenary': '#478dca',
+				'custom-nonary': '#4a4b97',
+				'custom-denary': '#e77a82',
+				'custom-undenary': '#f29140',
+				//'custom-Tredenary': '#',
+				//'custom-Quattuordenary': '#',
+				//'custom-Quinquadrenary': '#',
+				//'custom-Sedenary': '#',
 			
 			},
 			fontSize: {
