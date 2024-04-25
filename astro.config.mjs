b36b046c7872a9f060c5923885db4c106885e939
CarlosEbb <167257281+cebolivarb@users.noreply.github.com>
2024-04-25 11:27:20 -0400

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
