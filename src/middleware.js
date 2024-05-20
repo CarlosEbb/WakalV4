d7da9c7b508647b3b72008e136e6cc2f4395e6cb
CarlosEbb <167257281+cebolivarb@users.noreply.github.com>
2024-05-20 09:38:57 -0400

    cambios

--- src/middleware.js
+++ src/middleware.js
@@ -1,66 +1,53 @@
 import { sequence } from "astro:middleware";
 import jwt from 'jsonwebtoken';
 
-export function onRequest ({ locals, request, url, cookies}, next) {
+export async function onRequest({ locals, request, url, cookies }, next) {
     let isPrivate = [
         '/auth/selectedAction',
     ];
     let path = url.pathname;
 
     if (path.includes('/dashboard') || isPrivate.includes(path)) {
-        
+
         let session = cookies.get('session');
         if (!session) {
-            const modifiedResponse = new Response(nextResponse.body, {
-                ...nextResponse.headers,
+            return new Response(null, {
+                status: 302,
                 headers: {
-                ...nextResponse.headers.headers,
-                Location: '/',
-                },
-            });
-
-            return modifiedResponse;
-        }
-        session = JSON.parse(session.value);
-       
-        try {
-            jwt.verify(session.token, 'secretKey', (err, decodedToken) => {
-                if (err) {
-
-                    const modifiedResponse = new Response(nextResponse.body, {
-                        ...nextResponse.headers,
-                        headers: {
-                        ...nextResponse.headers.headers,
-                        Location: '/',
-                        },
-                    });
-
-                    return modifiedResponse;
-                  
-                } else {
-                    // Agrega el token a locals
-                    locals.token = session.token;
-                    locals.rol_id = session.rol_id;
-                    locals.cliente_id = session.cliente_id;
-                    locals.logo = session.logo;
-
-                    return next();
+                    Location: '/'
                 }
             });
-        } catch (err) {
-            const modifiedResponse = new Response(nextResponse.body, {
-                ...nextResponse.headers,
-                headers: {
-                ...nextResponse.headers.headers,
-                Location: '/',
-                },
+        }
+
+        session = JSON.parse(session.value);
+
+        try {
+            const decodedToken = await new Promise((resolve, reject) => {
+                jwt.verify(session.token, 'secretKey', (err, decoded) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(decoded);
+                    }
+                });
             });
 
-            return modifiedResponse;
+            // Agrega el token a locals
+            locals.token = session.token;
+            locals.rol_id = session.rol_id;
+            locals.cliente_id = session.cliente_id;
+            locals.logo = session.logo;
+
+            return next();
+        } catch (err) {
+            return new Response(null, {
+                status: 302,
+                headers: {
+                    Location: '/'
+                }
+            });
         }
     } else {
         return next();
-       
     }
-
 }
